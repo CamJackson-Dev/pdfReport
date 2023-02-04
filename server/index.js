168eaa8b682ed27bb58df5c37614d010a9e6ebf4
Cam Jackson <cameroncj86@gmail.com>
2023-02-04 21:29:57 +1000

    deploying

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/create-pdf", (req, res) => {
-  res.set("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   pdf.create(pdfTemplate(req.body), {}).toFile("result.pdf", (err) => {
     if (err) {
       res.send(Promise.reject());
@@ -27,5 +28,18 @@ app.post("/create-pdf", (req, res) => {
 app.get("/fetch-pdf", (req, res) => {
   res.sendFile(`${__dirname}/result.pdf`);
 });
+
+// 405 & 404 error catcher
+app.use("", (req, _, next) => {
+  const err = buildError(app, req);
+  if (!err) return next();
+  return next(err);
+});
+// 405 & 404 error catcher
+
+// Error handling
+app.use((err, _, res, __) =>
+  res.status(err.status).json({ message: err.message })
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
